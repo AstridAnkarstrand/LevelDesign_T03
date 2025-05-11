a372cef5da3020bb1bbf3d3a05e68f192032e8de
AstridAnkarstrand <astrid.ankarstrand@edu.futuregames.nu>
2025-05-11 14:05:50 +0200

    Changes I forgot to push

--- LevelDesignPart1/Assets/Astrid/Scripts/TriggerZone.cs
+++ LevelDesignPart1/Assets/Astrid/Scripts/TriggerZone.cs
@@ -11,29 +11,32 @@ public class TriggerZone : MonoBehaviour
 
     void Awake()
     {
+        // To stop the Update() from being called until object's been triggered.
         enabled = false;
     }
 
     void OnTriggerEnter(Collider other)
     {
+        // Check if it's the player colliding
         if (other.GetComponent<CharacterController>() == null) return;
 
+        // Disable the collider to avoid being triggered multiple times.
         gameObject.GetComponent<Collider>().enabled = false;
+
         // Do stuff
         onFirstEnter.Invoke();
+
+        // Start timer
         enabled = true;
     }
 
     void Update()
     {
-        if (!enabled) return;
-
         currTime += Time.deltaTime;
 
         if (currTime > timer)
         {
             onEnd.Invoke();
-            enabled = false;
             Destroy(gameObject);
         }
     }
